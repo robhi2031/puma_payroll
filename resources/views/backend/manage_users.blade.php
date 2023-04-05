c2d4cc8eeff9c303998c4dcc1f38fe72c43ded47
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-04-05 13:58:13 +0800

    Update manage_users.blade.php

--- resources/views/backend/manage_users.blade.php
+++ resources/views/backend/manage_users.blade.php
@@ -1,7 +1,7 @@
 @extends('backend.layouts', ['activeMenu' => 'SETTINGS', 'activeSubMenu' => 'Users'])
 @section('content')
 <!--begin::Card Form-->
-<div class="card" id="card-formUser">
+<div class="card" id="card-formUser" style="display: none;">
     <!--begin::Card header-->
     <div class="card-header">
         <div class="card-title"></div>
@@ -19,7 +19,106 @@
         <div class="card-body">
             <!--begin::Row-->
             <div class="row">
-                
+                <div class="col-md-12">
+                    <!--begin::Input group-->
+                    <div class="mb-3">
+                        <div class="row">
+                            <label class="col-md-12 col-form-label required fw-bold fs-6" for="avatar">Foto</label>
+                            <div class="col-md-12" id="iGroup-userThumb">
+                                <div class="image-input image-input-outline" data-kt-image-input="true" style="background-image: url('{{ asset('/dist/img/avatar-blank.svg') }}')">
+                                    <!--begin::Preview existing avatar-->
+                                    <div class="image-input-wrapper w-125px h-125px"></div>
+                                    <!--end::Preview existing avatar-->
+                                    <label class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="change" data-bs-toggle="tooltip" title="Ubah foto user">
+                                        <i class="bi bi-pencil-fill fs-7"></i>
+                                        <input type="file" name="avatar" id="avatar" accept=".png, .jpg, .jpeg" />
+                                        <input type="hidden" name="avatar_remove" id="avatar_remove" value="1" />
+                                    </label>
+                                    <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" id="btn-cancelUserThumb" data-kt-image-input-action="cancel" data-bs-toggle="tooltip" title="Batalkan perubahan foto user">
+                                        <i class="bi bi-x fs-2"></i>
+                                    </span>
+                                    <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="remove" data-bs-toggle="tooltip" title="Hapus foto user">
+                                        <i class="bi bi-x fs-2"></i>
+                                    </span>
+                                </div>
+                            </div> 
+                            <div class="col-md-12 form-text">*) Jenis file yang diizinkan: <code>png, jpg, jpeg</code> | Ukuran file Maks: <code>2MB</code></div>
+                        </div>
+                    </div>
+                    <!--end::Input group-->
+                    <!--begin::Input group-->
+                    <div class="mb-3" id="iGroup-role">
+                        <label class="col-form-label required fw-bold fs-6" for="role">Role User</label>
+                        <select class="show-tick form-select-solid" data-width="100%" data-style="btn-sm btn-primary" name="role" id="role" data-container="body" title="Pilih role User ..."></select>
+                    </div>
+                    <!--end::Input group-->
+                    <!--begin::Input group-->
+                    <div class="mb-3">
+                        <label class="col-form-label required fw-bold fs-6" for="name">Nama</label>
+                        <input type="text" class="form-control form-control-solid" name="name" id="name" maxlength="150" placeholder="Isikan nama user ..." />
+                    </div>
+                    <!--end::Input group-->
+                    <!--begin::Input group-->
+                    <div class="mb-3">
+                        <label class="col-form-label required fw-bold fs-6" for="username">Username</label>
+                        <div class="input-group input-group-solid">
+                            <span class="input-group-text"><i class="las la-user fs-1"></i></span>
+                            <input type="text" class="form-control form-control-solid no-space" name="username" id="username" maxlength="50" placeholder="Isikan username ..." />
+                        </div>
+                        <div class="form-text">*) Tanpa spasi, contoh: <code>andre123</code></div>
+                    </div>
+                    <!--end::Input group-->
+                    <!--begin::Input group-->
+                    <div class="mb-3">
+                        <label class="col-form-label required fw-bold fs-6" for="email">Email</label>
+                        <div class="input-group input-group-solid mb-2">
+                            <span class="input-group-text"><i class="las la-envelope fs-1"></i></span>
+                            <input type="text" class="form-control form-control-lg form-control-solid no-space" name="email" id="email" placeholder="Isikan email user ..." />
+                        </div>
+                        <div class="form-text">*) Pastikan email sesuai format dan masih aktif digunakan, contoh: <code>andre123@gmail.com</code></div>
+                    </div>
+                    <!--end::Input group-->
+                    <!--begin::Input group-->
+                    <div class="mb-3">
+                        <label class="col-form-label required fw-bold fs-6" for="phone_number">No. Telpon/ Hp</label>
+                        <div class="input-group input-group-solid mb-2">
+                            <span class="input-group-text"><i class="las la-phone fs-1"></i></span>
+                            <input type="text" class="form-control form-control-lg form-control-solid" name="phone_number" id="phone_number" placeholder="Isikan No. Telpon/Hp user ..." />
+                        </div>
+                        <div class="form-text">*) Pastikan No. Telpon/Hp sesuai format dan masih aktif digunakan, contoh: <code>+6283122222222</code></div>
+                    </div>
+                    <!--end::Input group-->
+                    <!--begin::Input group-->
+                    <div class="mb-3 password-group">
+                        <label class="col-form-label required fw-bold fs-6" for="pass_user">Password</label>
+                        <div class="input-group input-group-solid">
+                            <input type="password" class="form-control form-control-lg form-control-solid no-space" name="pass_user" id="pass_user" minlength="6" placeholder="Isikan password user ..." />
+                            <span class="input-group-text cursor-pointer btn-showPass" title="Sembunyikan password"><i class="las la-eye-slash fs-1"></i></span>
+                        </div>
+                        <div class="form-text">*) Tanpa spasi, Panjang karakter minimal 6 | contoh: <code>User.123456</code></div>
+                    </div>
+                    <!--end::Input group-->
+                    <!--begin::Input group-->
+                    <div class="mb-3 password-group">
+                        <label class="col-form-label required fw-bold fs-6" for="repass_user">Ulangi Password</label>
+                        <div class="input-group input-group-solid">
+                            <input type="password" class="form-control form-control-lg form-control-solid no-space" name="repass_user" id="repass_user" minlength="6" placeholder="Ulangi password user ..." />
+                            <span class="input-group-text cursor-pointer btn-showPass" title="Sembunyikan password"><i class="las la-eye-slash fs-1"></i></span>
+                        </div>
+                        <div class="form-text">*) Tanpa spasi, Panjang karakter minimal 6 | contoh: <code>User.123456</code></div>
+                    </div>
+                    <!--end::Input group-->
+                    <!--begin::Input group-->
+                    <div class="mb-3" id="iGroup-isActive" style="display: none;">
+                        <label class="col-form-label fw-bold fs-6" for="is_active">Status User</label>
+                        <div class="form-check form-switch form-check-custom form-check-solid">
+                            <input class="form-check-input" type="checkbox" id="is_active" name="is_active" />
+                            <label class="form-check-label" for="is_active"></label>
+                        </div>
+                        <div class="form-text">*) Jika <code>Tidak Aktif</code>, User tidak bisa login ke sistem</div>
+                    </div>
+                    <!--end::Input group-->
+                </div>
             </div>
             <!--end::Row-->
         </div>
@@ -35,7 +134,7 @@
 </div>
 <!--end::Card Form-->
 <!--begin::List Table Data-->
-<div class="card shadow" id="card-dtUser">
+<div class="card shadow" id="card-dtUsers">
     <!--begin::Card header-->
     <div class="card-header">
         <div class="card-title">
@@ -73,13 +172,11 @@
                     <tr class="fw-bolder text-uppercase bg-light">
                         <th class="text-center align-middle px-2 border-bottom-2 border-gray-200">No.</th>
                         <th class="border-bottom-2 border-gray-200 align-middle px-2"></th><!--Level User Group-->
-                        <th class="align-middle px-2 border-bottom-2 border-gray-200">Name</th>
-                        <th class="align-middle px-2 border-bottom-2 border-gray-200">Username</th>
+                        <th class="align-middle px-2 border-bottom-2 border-gray-200">User</th><!--Foto, Nama, Username-->
                         <th class="align-middle px-2 border-bottom-2 border-gray-200">Email</th>
                         <th class="align-middle px-2 border-bottom-2 border-gray-200">Telp./ Hp</th>
-                        <th class="align-middle px-2 border-bottom-2 border-gray-200">Has Child</th>
-                        <th class="align-middle px-2 border-bottom-2 border-gray-200">CRUD</th>
-                        <th class="align-middle px-2 border-bottom-2 border-gray-200">Order Line</th>
+                        <th class="align-middle px-2 border-bottom-2 border-gray-200">Active</th>
+                        <th class="align-middle px-2 border-bottom-2 border-gray-200">Last Login</th>
                         <th class="text-center align-middle px-2 border-bottom-2 border-gray-200">Aksi</th>
                     </tr>
                 </thead>
