9909166c365ec40e3c556beb613b0dbeac5e2bc1
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-05-05 00:28:12 +0800

    Create 2023_03_19_132118_create_company_table.php

---
+++ database/migrations/2023_03_19_132118_create_company_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompanyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 225);
+            $table->string('short_description', 225);
+            $table->text('logo')->nullable();
+            $table->text('profile')->nullable();
+            $table->string('phone_number', 25)->nullable();
+            $table->string('email')->nullable();
+            $table->string('office_address')->nullable();
+            $table->text('office_address_coordinate')->nullable();
+            $table->integer('user_updated')->nullable();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('NULL on update CURRENT_TIMESTAMP'))->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company');
+    }
+}
