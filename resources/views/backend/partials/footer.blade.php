ab297279d5e596ef240a0bb3fd13015b688363a8
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-05-04 10:18:34 +0800

    Update footer.blade.php

--- resources/views/backend/partials/footer.blade.php
+++ resources/views/backend/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <!--begin::Footer-->
 <div id="kt_app_footer" class="app-footer ">
     <!--begin::Footer container-->
-    <div class="app-container  container-fluid d-flex flex-column flex-md-row flex-center py-3 ">
+    <div class="app-container container-fluid d-flex flex-column flex-md-row flex-center py-3 ">
         <!--begin::Copyright-->
         <div class="text-dark order-2 order-md-1 w-100 text-center copyRight" id="footerCopyright">
             <div class="h3 placeholder-glow">
