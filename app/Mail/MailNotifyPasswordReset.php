0ddf9c3001a51ec61b6c407b41670b48488df3d9
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-05-04 14:17:44 +0800

    Create MailNotifyPasswordReset.php

---
+++ app/Mail/MailNotifyPasswordReset.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class MailNotifyPasswordReset extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('noreply.hrdpju@gmail.com', 'noreply - '.$this->data["systemInfo"]->short_name)
+            ->subject($this->data["subject"])
+            ->view('mail_templated.password_reset')->with("data", $this->data);
+    }
+}
