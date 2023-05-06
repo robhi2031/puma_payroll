70be4c21192561160048975faba5d0ecd55bcaa7
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-05-06 14:25:13 +0800

    Create 2023_05_05_132001_create_bank_account_table.php

---
+++ database/migrations/2023_05_05_132001_create_bank_account_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBankAccountTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_account', function (Blueprint $table) {
+            $table->id();
+            $table->string('bank_name', 225);
+            $table->string('account_name', 225);
+            $table->string('account_number', 225);
+            $table->string('is_active', 1)->default('Y');
+            $table->integer('user_add');
+            $table->integer('user_updated')->nullable();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('NULL on update CURRENT_TIMESTAMP'))->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_account');
+    }
+}
