75657a63461c317687cc37d78cfe3e761c2582c9
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-05-06 14:25:10 +0800

    Update 2023_03_19_132118_create_company_table.php

--- database/migrations/2023_03_19_132118_create_company_table.php
+++ database/migrations/2023_03_19_132118_create_company_table.php
@@ -23,6 +23,13 @@ class CreateCompanyTable extends Migration
             $table->string('email')->nullable();
             $table->string('office_address')->nullable();
             $table->text('office_address_coordinate')->nullable();
+            $table->string('npwp')->nullable();
+            $table->string('no_jamsostek')->nullable();
+            $table->unsignedBigInteger('fid_bank_account')->nullable(); // => ID=1 for Company Bank Account
+            $table->foreign('fid_bank_account')
+                ->references('id')
+                ->on('bank_account')
+                ->onDelete('cascade');
             $table->integer('user_updated')->nullable();
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('NULL on update CURRENT_TIMESTAMP'))->nullable();
