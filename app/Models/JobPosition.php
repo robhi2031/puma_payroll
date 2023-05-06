10bd9c1dd544a618513e05707cd4ed67ab931574
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-05-06 16:16:51 +0800

    Create JobPosition.php

---
+++ app/Models/JobPosition.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JobPosition extends Model
+{
+    use HasFactory;
+
+    protected $table = 'job_position';
+    
+    protected $guarded = ['id'];
+
+    public $timestamps = false;
+}
