e21c322aa4c436b58fae4bff13d30983cdc5cf1e
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-05-06 14:25:18 +0800

    Create 2023_05_05_132003_create_job_position_table.php

---
+++ database/migrations/2023_05_05_132003_create_job_position_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobPositionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_position', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 8);
+            $table->string('name', 225);
+            $table->integer('user_add');
+            $table->integer('user_updated')->nullable();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('NULL on update CURRENT_TIMESTAMP'))->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_position');
+    }
+}
