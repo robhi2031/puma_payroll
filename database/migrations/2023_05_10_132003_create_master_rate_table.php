f30864d9e34459fb1a05347e7ec92f3729ae6b65
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-05-11 11:58:09 +0800

    Create 2023_05_10_132003_create_master_rate_table.php

---
+++ database/migrations/2023_05_10_132003_create_master_rate_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMasterRateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('master_rate', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 8); // uniq
+            $table->string('name', 225);
+            $table->string('value', 225);
+            $table->integer('user_add');
+            $table->integer('user_updated')->nullable();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('NULL on update CURRENT_TIMESTAMP'))->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('master_rate');
+    }
+}
