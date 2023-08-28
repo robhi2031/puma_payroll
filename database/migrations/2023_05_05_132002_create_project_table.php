26ccd64077d6b19bd244bb4f67834ffa722795a2
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-08-28 22:37:04 +0800

    Update 2023_05_05_132002_create_project_table.php

--- database/migrations/2023_05_05_132002_create_project_table.php
+++ database/migrations/2023_05_05_132002_create_project_table.php
@@ -22,7 +22,8 @@ class CreateProjectTable extends Migration
             $table->string('client', 255);
             $table->date('start_date');
             $table->date('end_date');
-            $table->string('status', 50)->default('Not Started'); //Not Started || In Progress || Completed || Stop
+            // $table->string('status', 50)->default('Not Started'); //Not Started || In Progress || Completed || Stop
+            $table->tinyInteger('status', 1)->default(0); //1 => Aktif; 0 => Tidak Aktif
             $table->integer('user_add');
             $table->integer('user_updated')->nullable();
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
