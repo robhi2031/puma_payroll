8e22c9b6cf1c48c518fe28d84a17ba7ddaef9b38
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2023-08-28 22:37:02 +0800

    Update ProjectController.php

--- app/Http/Controllers/Backend/ProjectController.php
+++ app/Http/Controllers/Backend/ProjectController.php
@@ -67,7 +67,8 @@ class ProjectController extends Controller
     {
         if(isset($request->idp)){
             try {
-                $getRow = Project::selectRaw("project.*, DATE_FORMAT(project.start_date, '%d/%m/%Y') AS start_date_indo, DATE_FORMAT(project.end_date, '%d/%m/%Y') AS end_date_indo")->where('id', $request->idp)->first();
+                // $getRow = Project::selectRaw("project.*, DATE_FORMAT(project.start_date, '%d/%m/%Y') AS start_date_indo, DATE_FORMAT(project.end_date, '%d/%m/%Y') AS end_date_indo")->where('id', $request->idp)->first();
+                $getRow = Project::whereId($request->idp)->first();
                 if($getRow != null){
                     return jsonResponse(true, 'Success', 200, $getRow);
                 } else {
@@ -81,18 +82,22 @@ class ProjectController extends Controller
         } else {
             $data = Project::selectRaw("project.*, 'action' as action")->orderByDesc('id')->get();
             $output = Datatables::of($data)->addIndexColumn()
-                ->addColumn('project_date', function ($row) {
+                /* ->addColumn('project_date', function ($row) {
                     $dateCustom = 'Mulai: '.mediumdate_indo($row->start_date).'<br/>Selesai: '.mediumdate_indo($row->end_date);
                     return $dateCustom;
-                })
+                }) */
                 ->editColumn('status', function ($row) {
-                    $statusCustom = '<span data-kt-element="status" class="badge badge-light-warning" data-bs-toggle="tooltip" title="Belum berjalan"><i class="bi bi-dash-circle me-1 text-warning"></i>Not Started</span>';
+                    $statusCustom = '<span data-kt-element="status" class="badge badge-light-danger" data-bs-toggle="tooltip" title="Tidak Aktif"><i class="bi bi-sign-stop me-1 text-danger"></i>Tidak Aktif</span>';
+                    /* $statusCustom = '<span data-kt-element="status" class="badge badge-light-warning" data-bs-toggle="tooltip" title="Belum berjalan"><i class="bi bi-dash-circle me-1 text-warning"></i>Not Started</span>';
                     if($row->status == 'In Progress') {
                         $statusCustom = '<span data-kt-element="status" class="badge badge-light-info" data-bs-toggle="tooltip" title="Sedang berjalan"><i class="bi bi-bootstrap-reboot text-info me-1"></i>In Progress</span>';
                     } if($row->status == 'Completed') {
                         $statusCustom = '<span data-kt-element="status" class="badge badge-light-primary" data-bs-toggle="tooltip" title="Selesai"><i class="bi bi-check2-circle text-primary me-1"></i>Completed</span>';
                     } if($row->status == 'Stop') {
                         $statusCustom = '<span data-kt-element="status" class="badge badge-light-danger" data-bs-toggle="tooltip" title="Berhenti"><i class="bi bi-sign-stop text-danger me-1"></i>Stop</span>';
+                    } */
+                    if($row->status == 1) {
+                        $statusCustom = '<span data-kt-element="status" class="badge badge-light-primary" data-bs-toggle="tooltip" title="Aktif"><i class="bi bi-check2-circle text-primary me-1"></i>Aktif</span>';
                     }
                     return $statusCustom;
                 })
@@ -100,7 +105,8 @@ class ProjectController extends Controller
                     $btnEdit = '<button type="button" class="btn btn-icon btn-circle btn-sm btn-dark mb-1" data-bs-toggle="tooltip" title="Edit data!" onclick="_editProject('."'".$row->id."'".');"><i class="la la-edit fs-3"></i></button>';
                     return $btnEdit;
                 })
-                ->rawColumns(['project_date', 'status', 'action'])
+                // ->rawColumns(['project_date', 'status', 'action'])
+                ->rawColumns(['status', 'action'])
                 ->make(true);
     
             return $output;
@@ -115,36 +121,36 @@ class ProjectController extends Controller
     public function store(Request $request) {
         $userSesIdp = Auth::user()->id;
         $form = [
-            'code' => 'required|max:12',
+            // 'code' => 'required|max:12',
             'name' => 'required|max:225',
             'desc' => 'required|max:255',
             'client' => 'required|max:225',
             'location' => 'required|max:225',
-            'start_date' => 'required|max:10',
-            'end_date' => 'required|max:10',
+            // 'start_date' => 'required|max:10',
+            // 'end_date' => 'required|max:10',
             'status' => 'required',
         ];
         DB::beginTransaction();
         $request->validate($form);
         try {
             $code = strtoupper($request->code);
-            if(Project::where('code', $code)->exists()) {
+            /* if(Project::where('code', $code)->exists()) {
                 addToLog('Data cannot be saved, the same project code already exists in the system');
                 return jsonResponse(false, 'Gagal menambahkan data, kode proyek yang sama sudah ada pada sistem. Coba gunakan kode yang berbeda', 200, array('error_code' => 'code_available'));
             }
             $start_date = str_replace('/', '-', $request->start_date);
             $start_date = date('Y-m-d', strtotime($start_date));
             $end_date = str_replace('/', '-', $request->end_date);
-            $end_date = date('Y-m-d', strtotime($end_date));
+            $end_date = date('Y-m-d', strtotime($end_date)); */
             //array data
             $data = array(
-                'code' => $code,
+                // 'code' => $code,
                 'name' => $request->name,
                 'desc' => $request->desc,
                 'client' => $request->client,
                 'location' => $request->location,
-                'start_date' => $start_date,
-                'end_date' => $end_date,
+                // 'start_date' => $start_date,
+                // 'end_date' => $end_date,
                 'status' => $request->status,
                 'user_add' => $userSesIdp
             );
@@ -168,12 +174,19 @@ class ProjectController extends Controller
     public function update(Request $request) {
         $userSesIdp = Auth::user()->id;
         $form = [
-            'name' => 'required|max:50',
+            // 'code' => 'required|max:12',
+            'name' => 'required|max:225',
+            'desc' => 'required|max:255',
+            'client' => 'required|max:225',
+            'location' => 'required|max:225',
+            // 'start_date' => 'required|max:10',
+            // 'end_date' => 'required|max:10',
+            'status' => 'required',
         ];
         DB::beginTransaction();
         $request->validate($form);
         try {
-            $code = strtoupper($request->code);
+            /* $code = strtoupper($request->code);
             if(Project::where('code', $code)->where('id', '!=' , $request->id)->exists()) {
                 addToLog('Data cannot be updated, the same project code already exists in the system');
                 return jsonResponse(false, 'Gagal memperbarui data, kode proyek yang sama sudah ada pada sistem. Coba gunakan kode yang berbeda', 200, array('error_code' => 'code_available'));
@@ -181,16 +194,16 @@ class ProjectController extends Controller
             $start_date = str_replace('/', '-', $request->start_date);
             $start_date = date('Y-m-d', strtotime($start_date));
             $end_date = str_replace('/', '-', $request->end_date);
-            $end_date = date('Y-m-d', strtotime($end_date));
+            $end_date = date('Y-m-d', strtotime($end_date)); */
             //array data
             $data = array(
-                'code' => $code,
+                // 'code' => $code,
                 'name' => $request->name,
                 'desc' => $request->desc,
                 'client' => $request->client,
                 'location' => $request->location,
-                'start_date' => $start_date,
-                'end_date' => $end_date,
+                // 'start_date' => $start_date,
+                // 'end_date' => $end_date,
                 'status' => $request->status,
                 'user_updated' => $userSesIdp
             );
